2b5dc515e96c9db9e2a2a60126acaa9b64fd9582
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2023-02-23 00:00:19 +0300

    keyup-event

--- example/src/app/product/product.component.ts
+++ example/src/app/product/product.component.ts
@@ -16,4 +16,19 @@ export class ProductComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onKeyUp($event) {
+    if ($event.keyCode === 13) {
+      console.log("enter was pressed");
+    }
+  }
+  onKeyUp2() {
+    console.log("enter was pressed onKeyUp2");
+  }
+  onKeyUp3($event) {
+    console.log($event.target.value);
+  }
+  onKeyUp4(x) {
+    console.log(x);
+  }
+
 }
