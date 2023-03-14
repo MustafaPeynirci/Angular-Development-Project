ce7d073fc9c980a6d77257fefbbc5396e4576cdd
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2023-03-15 00:23:13 +0300

    specific-validation-errors

---
+++ example/src/app/image.validators.ts
@@ -0,0 +1,15 @@
+import { AbstractControl, ValidationErrors } from "@angular/forms";
+
+export class ImageValidators {
+    static isValidExtension(control: AbstractControl): ValidationErrors | null {
+        const v = control.value as string
+        if (v.endsWith(".jpg") || v.endsWith(".jpeg") || v.endsWith(".png")) {
+            return null
+        }
+        else {
+            return {
+                wrongExtension: true
+            }
+        }
+    }
+}
--- example/src/app/product2/product2.component.ts
+++ example/src/app/product2/product2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ImageValidators } from '../image.validators';
 
 @Component({
   selector: 'app-product2',
@@ -9,10 +10,17 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class Product2Component implements OnInit {
 
   productForm = new FormGroup({
-    name: new FormControl("", [Validators.required, Validators.minLength(5)]),
+    name: new FormControl("", [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(20)
+    ]),
     description: new FormControl("", Validators.required),
     price: new FormControl("", Validators.required),
-    imageUrl: new FormControl("", Validators.required)
+    imageUrl: new FormControl("", [
+      Validators.required,
+      ImageValidators.isValidExtension
+    ])
   })
 
   constructor() { }
@@ -29,9 +37,11 @@ export class Product2Component implements OnInit {
       price: "9400"
     })
   }
-
   get name() {
     return this.productForm.get('name')
+  }
+  get imageUrl() {
+    return this.productForm.get('imageUrl')
   }
 
 }
